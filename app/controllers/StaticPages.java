a0e19e3d6cb6287e825376b2923a61b753bc37f7
Benjamin Bouvier <public@benj.me>
2013-02-27 16:22:40 +0100

    ajout: page "à propos"

--- app/controllers/StaticPages.java
+++ app/controllers/StaticPages.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.a_propos;
 import views.html.index;
 import views.html.main;
 
@@ -9,6 +10,10 @@ public class StaticPages extends Controller {
 
     public static Result index() {
         return ok(main.render("Accueil", index.render()));
+    }
+
+    public static Result apropos() {
+        return ok(main.render("A propos", a_propos.render()));
     }
 
 }
