df047c2767707707ae8b2360eefcd65fa561ef3e
Benjamin Bouvier <public@benj.me>
2013-02-27 16:03:17 +0100

    ajout: index + fichiers statiques

--- app/controllers/StaticPages.java
+++ app/controllers/StaticPages.java
@@ -3,11 +3,12 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.main;
 
-public class Application extends Controller {
+public class StaticPages extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(main.render("Accueil", index.render()));
     }
 
 }
