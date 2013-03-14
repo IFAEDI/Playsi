f24bab8cd2c8df6613478f294d7e9b1437faf829
Benjamin Bouvier <public@benj.me>
2013-03-14 15:34:46 +0100

    ajout: test sauvegarde contact et utilisateur

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,13 +1,15 @@
 import controllers.Utils.Constantes;
 import controllers.routes;
-import models.Stage;
+import models.*;
 import org.codehaus.jackson.node.ObjectNode;
 import org.junit.Before;
 import org.junit.Test;
+import play.db.ebean.Model;
 import play.libs.Json;
 import play.mvc.Result;
 import services.StageService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -75,5 +77,91 @@ public class ApplicationTest {
         assertThat( collectedJson.has(Constantes.JSON_STATUT) );
         assertThat( collectedJson.get(Constantes.JSON_STATUT).asText() ).isNotEqualTo(Constantes.JSON_OK_STR);
         assertThat( ! session(result).containsKey(Constantes.SESSION_ID) );
+    }
+
+    @Test
+    public void testSaveContact() {
+
+        Entreprise entreprise = new Entreprise();
+        entreprise.setNom("Corp");
+        entreprise.setDescription("Une entreprise spécialisée dans qqch de spécial.");
+        entreprise.setSecteur("Conseil");
+        entreprise.save();
+
+        ContactEntreprise ce1 = new ContactEntreprise();
+        ce1.setNom("Patrick");
+        ce1.setPrenom("John");
+        ce1.setCommentaire("Il a la classe avec un nom pareil.");
+
+        List<Mail> mce1 = new ArrayList<Mail>();
+        Mail ce1m1 = new Mail();
+        ce1m1.setIntitule("Personnel");
+        ce1m1.setEmail("john@patrick.com");
+        mce1.add(ce1m1);
+
+        Mail ce1m2 = new Mail();
+        ce1m2.setIntitule("Pro");
+        ce1m2.setEmail("john.patrick@pro.org");
+        mce1.add(ce1m2);
+
+        ce1.setMails(mce1);
+        assertThat(ce1.getMails()).isNotNull();
+
+        Model.Finder<Long, Entreprise> finder = new Model.Finder<Long, Entreprise>(Long.class, Entreprise.class);
+        Entreprise chargee = finder.byId(1L);
+
+        assertThat(chargee).isNotNull();
+        assertThat(chargee.getNom()).isEqualTo("Corp");
+
+        List<ContactEntreprise> sesContacts = chargee.getContacts();
+        assertThat(sesContacts).isEmpty();
+        sesContacts = new ArrayList<ContactEntreprise>();
+        sesContacts.add(ce1);
+        chargee.setContacts(sesContacts);
+        chargee.save();
+
+        Entreprise rechargee = finder.byId(1L);
+        assertThat(rechargee).isNotNull();
+        assertThat(rechargee.getContacts()).isNotEmpty();
+        assertThat(rechargee.getContacts().size()).isEqualTo(1);
+        assertThat(rechargee.getContacts().get(0).getNom()).isEqualTo("Patrick");
+        assertThat(rechargee.getContacts().get(0).getMails()).isNotEmpty();
+        assertThat(rechargee.getContacts().get(0).getMails().size()).isEqualTo(2);
+        assertThat(rechargee.getContacts().get(0).getMails().get(0).getEmail()).isEqualTo("john@patrick.com");
+    }
+
+    @Test
+    public void testSaveUtilisateur() {
+
+        Utilisateur ce1 = new Utilisateur();
+        ce1.setNom("Patrick");
+        ce1.setPrenom("John");
+
+        assertThat(ce1.getId()).isNotNull();
+        assertThat(ce1.getId()).isEqualTo(2L); // déjà l'administrateur dans la base
+
+        List<Mail> mce1 = new ArrayList<Mail>();
+        Mail ce1m1 = new Mail();
+        ce1m1.setIntitule("Personnel");
+        ce1m1.setEmail("john@patrick.com");
+        mce1.add(ce1m1);
+
+        Mail ce1m2 = new Mail();
+        ce1m2.setIntitule("Pro");
+        ce1m2.setEmail("john.patrick@pro.org");
+        mce1.add(ce1m2);
+
+        ce1.setMails(mce1);
+        assertThat(ce1.getMails()).isNotNull();
+
+        ce1.save();
+
+        Model.Finder<Long, Utilisateur> finder = new Model.Finder<Long, Utilisateur>(Long.class, Utilisateur.class);
+        Utilisateur charge = finder.where().eq("nom", "Patrick").findUnique();
+
+        assertThat(charge).isNotNull();
+        assertThat(charge.getNom()).isEqualTo("Patrick");
+        assertThat(charge.getMails()).isNotEmpty();
+        assertThat(charge.getMails().get(0).getEmail()).isEqualTo("john@patrick.com");
     }
 }
